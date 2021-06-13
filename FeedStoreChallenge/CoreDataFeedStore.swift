a8890d51d859ccb969250e0c94616e968681fe64
Andreas Link <andreas.link@jamitlabs.com>
2021-06-13 10:24:24 +0200

    Retrieve delivers found values on non empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,14 +29,58 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		context.perform {
+			let request = NSFetchRequest<ManagedCache>(entityName: String(describing: ManagedCache.self))
+			let results = try! self.context.fetch(request)
+			if let cache = results.first {
+				completion(.found(feed: cache.feed.toLocal(), timestamp: cache.timestamp))
+			} else {
+				completion(.empty)
+			}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		fatalError("Must be implemented")
+		context.perform {
+			let newCache = ManagedCache(context: self.context)
+			newCache.timestamp = timestamp
+			newCache.feed = feed.toManaged(context: self.context)
+			try! self.context.save()
+			completion(nil)
+		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		fatalError("Must be implemented")
+	}
+}
+
+private extension NSOrderedSet {
+	func toLocal() -> [LocalFeedImage] {
+		array
+			.compactMap { $0 as? ManagedFeedImage }
+			.map { managedFeedImage in
+				return LocalFeedImage(
+					id: managedFeedImage.id,
+					description: managedFeedImage.imageDescription,
+					location: managedFeedImage.location,
+					url: managedFeedImage.url
+				)
+			}
+	}
+}
+
+private extension Array where Element == LocalFeedImage {
+	func toManaged(context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(
+			array: map { localFeedImage in
+				let managedFeedImage = ManagedFeedImage(context: context)
+				managedFeedImage.id = localFeedImage.id
+				managedFeedImage.imageDescription = localFeedImage.description
+				managedFeedImage.location = localFeedImage.location
+				managedFeedImage.url = localFeedImage.url
+				return managedFeedImage
+			}
+		)
 	}
 }
